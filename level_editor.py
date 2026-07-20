71851855d90d175230b275a3f0f661691ec13d0a
LC1C_22_moriya_yugo <k024g0016@g.neec.ac.jp>
2026-07-20 11:40:58 +0900

    01_10修正

--- level_editor.py
+++ level_editor.py
@@ -134,7 +134,7 @@ class MYADDON_OT_export_scene(bpy.types.Operator,bpy_extras.io_utils.ExportHelpe
         if "collider" in object:
             self.write_and_print(file,indent + "C %s" % object["collider"])
             temp_str = indent + "CC %f %f %f"
-            temp_str %= (object["collider_center"][0],object["collider_center"][1],object["collider_ceneter"][2])
+            temp_str %= (object["collider_center"][0],object["collider_center"][1],object["collider_center"][2])
             self.write_and_print(file,temp_str)
             temp_str = indent + "CS %f %f %f"
             temp_str %= (object["collider_size"][0],object["collider_size"][1],object["collider_size"][2])
